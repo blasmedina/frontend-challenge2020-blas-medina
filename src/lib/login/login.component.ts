54009e0f20356a4c93a617184de2d734a31a5755
Blas Medina <r.blas.m.c@gmail.com>
2020-06-26 00:10:08 -0400

    backup

--- src/lib/login/login.component.ts
+++ src/lib/login/login.component.ts
@@ -1,19 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
-import { FrontendChallenge2020BlasMedinaService } from '../frontend-challenge2020-blas-medina.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { FrontendChallenge2020BlasMedinaService } from "../frontend-challenge2020-blas-medina.service";
 
 // phbhelloworld@gmail.com
 // Nolodire20
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
   profileForm = this.fb.group({
-    username: ['phbhelloworld@gmail.com', Validators.required],
-    password: ['Nolodire20', Validators.required],
+    username: ["", Validators.required],
+    password: ["", Validators.required],
   });
 
   constructor(
